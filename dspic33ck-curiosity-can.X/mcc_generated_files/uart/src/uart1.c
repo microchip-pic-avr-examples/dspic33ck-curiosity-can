5b3f64056f95d513fd2cd1e62958c41256d8b1ac
I20885 <Poornachandra.Bhatta@microchip.com>
2022-04-05 15:22:49 +0530

    CC16SCRIP-3879 dsPIC33CK curiosity CAN 2.0 demo code

---
+++ dspic33ck-curiosity-can.X/mcc_generated_files/uart/src/uart1.c
@@ -0,0 +1,135 @@
+/**
+ * UART1 Generated Driver Source File
+ * 
+ * @file      uart1.c
+ *            
+ * @ingroup   uartdriver
+ *            
+ * @brief     This is the generated driver source file for the UART1 driver.
+ *            
+ * @version   Driver Version 1.1.0
+ *            
+ * @skipline  Device : dsPIC33CK256MP508
+*/
+
+/*
+© [2022] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+// Section: Included Files
+#include <xc.h>
+#include <stddef.h>
+#include "../uart1.h"
+
+// Section: Driver Interface
+
+const struct UART_INTERFACE UART1_Drv = {
+    .Initialize = &UART1_Initialize,
+    .Deinitialize = &UART1_Deinitialize,
+    .Read = &UART1_Read,
+    .Write = &UART1_Write,
+    .IsRxReady = &UART1_IsRxReady,
+    .IsTxReady = &UART1_IsTxReady,
+    .IsTxDone = &UART1_IsTxDone,
+    .RxCompleteCallbackRegister = NULL,
+    .TxCompleteCallbackRegister = NULL,
+    .TxCollisionCallbackRegister = NULL,
+    .FramingErrorCallbackRegister = NULL,
+    .OverrunErrorCallbackRegister = NULL,
+    .ParityErrorCallbackRegister = NULL,
+};
+
+// Section: UART1 APIs
+
+void UART1_Initialize(void)
+{
+/*    
+     Set the UART1 module to the options selected in the user interface.
+     Make sure to set LAT bit corresponding to TxPin as high before UART initialization
+*/
+    // URXEN ; RXBIMD ; UARTEN disabled; MOD Asynchronous 8-bit UART; UTXBRK ; BRKOVR ; UTXEN ; USIDL ; WAKE ; ABAUD ; BRGH enabled; 
+    U1MODE = 0x80;
+    // STSEL 1 Stop bit sent, 1 checked at RX; BCLKMOD ; SLPEN ; FLO ; BCLKSEL FOSC/2; C0EN ; RUNOVF ; UTXINV ; URXINV ; HALFDPLX ; 
+    U1MODEH = 0x0;
+    // OERIE ; RXBKIF ; RXBKIE ; ABDOVF ; OERR ; TXCIE ; TXCIF ; FERIE ; TXMTIE ; ABDOVE ; CERIE ; CERIF ; PERIE ; 
+    U1STA = 0x80;
+    // URXISEL ; UTXBE ; UTXISEL ; URXBE ; STPMD ; TXWRE ; 
+    U1STAH = 0x2E;
+    // BaudRate 9,615.385; Frequency 4,000,000 Hz; BRG 103; 
+    U1BRG = 0x67;
+    // BRG 0; 
+    U1BRGH = 0x0;
+    
+    U1MODEbits.UARTEN = 1;   // enabling UART ON bit
+    U1MODEbits.UTXEN = 1;
+    U1MODEbits.URXEN = 1;
+}
+
+void UART1_Deinitialize(void)
+{
+    U1MODE = 0x0;
+    U1MODEH = 0x0;
+    U1STA = 0x80;
+    U1STAH = 0x2E;
+    U1BRG = 0x0;
+    U1BRGH = 0x0;
+}
+
+uint8_t UART1_Read(void)
+{
+    while((U1STAHbits.URXBE == 1))
+    {
+        
+    }
+
+    if ((U1STAbits.OERR == 1))
+    {
+        U1STAbits.OERR = 0;
+    }
+    
+    return U1RXREG;
+}
+
+void UART1_Write(uint8_t txData)
+{
+    while(U1STAHbits.UTXBF == 1)
+    {
+        
+    }
+
+    U1TXREG = txData;    // Write the data byte to the USART.
+}
+
+bool UART1_IsRxReady(void)
+{
+    return (U1STAHbits.URXBE == 0);
+}
+
+bool UART1_IsTxReady(void)
+{
+    return ((!U1STAHbits.UTXBF) && U1MODEbits.UTXEN);
+}
+
+bool UART1_IsTxDone(void)
+{
+    return U1STAbits.TRMT;
+}
+
+
